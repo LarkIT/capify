a82cbd3cd0097340a862635b6234ded95f5d0cc6
Tommy McNeely <tommy@lark-it.com>
2016-03-08 22:26:27 -0700

    cleanup deployment configs

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -1,10 +1,10 @@
-# Staging Specific Deployment Configuration
+### Integration Specific Deployment Settings
 # ======================
 
 set :bundle_without, %w{production}.join(' ')
 
-#server 'stage.example.com',
+#server 'integ.example.com',
 #    user: fetch(:application),
-#    port: 1022
+#    port: 1022,
 #    roles: %w{web app},
 #    primary: true
